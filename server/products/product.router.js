8cda592e8dfdb4d9a7f66660904cf00369302215
Deshan <deshan.mclachlan@developersinstitute.co.nz>
2022-11-09 14:23:26 +1300

    In server - products - product.router file the getProducts query has been edited and now returns discount information from the database. Left joins added for product_discount and product_type. The data is being fetched correctly by client and is displaying discounts o items.

--- server/products/product.router.js
+++ server/products/product.router.js
@@ -13,18 +13,22 @@ const getProducts = async () => {
   try {
     const result = await db.query(
       `SELECT
-        p.id,
-        p.name,
-        p.description,
-        p.price,
-        pc.name AS "categoryName",
-        pi.name AS "imageName",
-        pi.description AS "imageDescription"
+      p.id,
+      p.name,
+      p.description,
+      p.price,
+      pc.name AS "categoryName",
+      pi.name AS "imageName",
+      pi.description AS "imageDescription",
+      dt.type AS "discountType",
+      pd.value AS "discountValue"
       FROM product p
       LEFT JOIN product_category pc ON p.product_category_id = pc.id
       LEFT JOIN product_image pi ON p.product_image_id = pi.id
+      LEFT JOIN product_discount pd ON pd.product_id = p.id
+      LEFT JOIN discount_type dt ON pd.discount_type_id = dt.id
       ORDER BY
-        p.id
+      p.id
       `
     );
     return result.rows;
