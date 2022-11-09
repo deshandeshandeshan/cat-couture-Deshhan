4ecfd4a43ea3480c23b6059acaa6f9e3cd08c558
Deshan <deshan.mclachlan@developersinstitute.co.nz>
2022-11-10 12:25:18 +1300

    A Controls component as well as a controls css file has been created inside client/src/components/products folder. I have populated the controls component with a Controls function that returns a product limit header as well as a select input field that contains 10, 25 and 50 options

---
+++ client/src/components/products/Controls.js
@@ -0,0 +1,14 @@
+import "./Controls.css";
+
+const Controls = () => {
+  return (
+    <div className="controls">
+      <p>Product limit</p>
+      <select className="controlsSelect">
+        <option>10</option>
+        <option>25</option>
+        <option>50</option>
+      </select>
+    </div>
+  );
+};
