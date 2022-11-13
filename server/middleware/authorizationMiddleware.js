efa8141d0ec3a283f4ef2bef0c481f91a678da47
Deshan <deshan.mclachlan@developersinstitute.co.nz>
2022-11-13 18:33:45 +1300

    In server/middleware/authorizationMiddleware file, configured the middleware. Inserted audience and issuerBaseURL

--- server/middleware/authorizationMiddleware.js
+++ server/middleware/authorizationMiddleware.js
@@ -1,8 +1,8 @@
 const { auth, requiredScopes } = require("express-oauth2-jwt-bearer");
 
 const checkJwt = auth({
-  audience: "",
-  issuerBaseURL: "",
+  audience: "https://catcouture.com",
+  issuerBaseURL: "https://dev-ejh1euf6.us.auth0.com/",
 });
 
 const checkScopes = requiredScopes("");
