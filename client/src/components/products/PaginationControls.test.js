e7d38f1b271ecee5eb2c71e8600b1e906f08d5e4
Deshan <deshan.mclachlan@developersinstitute.co.nz>
2022-11-17 09:00:30 +1300

    Made changes to pginationControls.test file, instead of passing in onPrev and onNext and mocking the function with jest.fn, I've instead passed in the setPage function. This is the function that sets the pages state and that is what is expected to be called when the button is clicked

--- client/src/components/products/PaginationControls.test.js
+++ client/src/components/products/PaginationControls.test.js
@@ -4,58 +4,35 @@ import PaginationControls from "./PaginationControls";
 
 describe("PaginationControls", () => {
   test("WHEN the user is on the first page of the products page, THEN the previous/back button of the pagination control will be disabled", () => {
-    const onPrev = jest.fn();
-    render(
-      <PaginationControls
-        onNext={() => {}}
-        onPrev={onPrev}
-        page={1}
-        totalPages={2}
-      />
-    );
+    const setPage = jest.fn();
+    render(<PaginationControls page={1} totalPages={2} setPage={setPage} />);
     const previousButton = screen.getByRole("button", {
       name: "Previous page",
     });
     expect(previousButton).toBeDisabled();
     userEvent.click(previousButton);
-    expect(onPrev).not.toHaveBeenCalled();
+    expect(setPage).not.toHaveBeenCalled();
   });
 
   test("WHEN the user is on the second page of the main product page, THEN the previous/back button of the pagination control will be enabled.", () => {
-    const setPage = (page) => page - 1;
-    const onPrev = jest.fn();
-    render(
-      <PaginationControls
-        onNext={() => {}}
-        onPrev={onPrev}
-        page={2}
-        totalPages={2}
-        setPage={setPage}
-      />
-    );
+    const setPage = jest.fn();
+    render(<PaginationControls page={2} totalPages={2} setPage={setPage} />);
     const previousButton = screen.getByRole("button", {
       name: "Previous page",
     });
     expect(previousButton).not.toBeDisabled();
     userEvent.click(previousButton);
-    expect(onPrev).toHaveBeenCalled();
+    expect(setPage).toHaveBeenCalled();
   });
 
   test("WHEN the user is on the last page of the main product page, THEN next button of the pagination control will be disabled.", () => {
-    const onNext = jest.fn();
-    render(
-      <PaginationControls
-        onNext={onNext}
-        onPrev={() => {}}
-        page={2}
-        totalPages={2}
-      />
-    );
+    const setPage = jest.fn();
+    render(<PaginationControls page={2} totalPages={2} setPage={setPage} />);
     const nextButton = screen.getByRole("button", {
       name: "Next page",
     });
     expect(nextButton).toBeDisabled();
     userEvent.click(nextButton);
-    expect(onNext).not.toHaveBeenCalled();
+    expect(setPage).not.toHaveBeenCalled();
   });
 });
