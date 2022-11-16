33026f62ea8f8a5686176dd329186b454c2bdc57
Deshan <deshan.mclachlan@developersinstitute.co.nz>
2022-11-17 09:02:07 +1300

    Made changes to productPage.test. I have moved the render out of the before each and created a setup function that is placed in each test assertion. Instead of using the waitforelementtoberemoved function I put waitFors in my tests. All client side tests now passing

--- client/src/components/products/ProductPage.test.js
+++ client/src/components/products/ProductPage.test.js
@@ -1,18 +1,13 @@
-import {
-  render,
-  screen,
-  waitForElementToBeRemoved,
-} from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import ProductPage from "./ProductPage";
 
-beforeEach(async () => {
-  render(<ProductPage />);
-  await waitForElementToBeRemoved(() => screen.queryByTitle(/loading/i));
-});
+const setup = () => render(<ProductPage />);
 
 describe("ProductPage", () => {
   test("WHEN a user goes to the Products page, THEN the pagination control will be displayed", () => {
+    setup();
+
     const previousPageButton = screen.getByRole("button", {
       name: "Previous page",
     });
@@ -22,15 +17,21 @@ describe("ProductPage", () => {
   });
 
   test("WHEN a user navigates to the first page of the Products page, THEN the previous/back button of the pagination control will be disabled", () => {
+    setup();
+
     const previousPageButton = screen.getByRole("button", {
       name: "Previous page",
     });
     expect(previousPageButton).toBeDisabled();
   });
 
-  test("WHEN the user navigates to the second page of the Products page, THEN the previous/back button of the pagination control will be enabled", () => {
+  test("WHEN the user navigates to the second page of the Products page, THEN the previous/back button of the pagination control will be enabled", async () => {
+    setup();
+
     const pageDisplay = screen.queryByText(/page/i);
-    expect(pageDisplay.textContent).toBe("Page 1 of 2");
+    await waitFor(() => {
+      expect(pageDisplay.textContent).toBe("Page 1 of 2");
+    });
     const nextPageButton = screen.getByRole("button", { name: "Next page" });
     userEvent.click(nextPageButton);
     const previousPageButton = screen.getByRole("button", {
@@ -39,9 +40,13 @@ describe("ProductPage", () => {
     expect(previousPageButton).not.toBeDisabled();
   });
 
-  test("WHEN the user navigates to the last page of the Products page, THEN next button of the pagination control will be disabled", () => {
+  test("WHEN the user navigates to the last page of the Products page, THEN next button of the pagination control will be disabled", async () => {
+    setup();
+
     const pageDisplay = screen.queryByText(/page/i);
-    expect(pageDisplay.textContent).toBe("Page 1 of 2");
+    await waitFor(() => {
+      expect(pageDisplay.textContent).toBe("Page 1 of 2");
+    });
     const nextPageButton = screen.getByRole("button", {
       name: "Next page",
     });
@@ -49,9 +54,13 @@ describe("ProductPage", () => {
     expect(nextPageButton).toBeDisabled();
   });
 
-  test("WHEN a user goes to the Products page, THEN the current page will be highlighted in the pagination control", () => {
+  test("WHEN a user goes to the Products page, THEN the current page will be highlighted in the pagination control", async () => {
+    setup();
+
     const pageDisplay = screen.queryByText(/page/i);
-    expect(pageDisplay.textContent).toBe("Page 1 of 2");
+    await waitFor(() => {
+      expect(pageDisplay.textContent).toBe("Page 1 of 2");
+    });
     const nextPageButton = screen.getByRole("button", { name: "Next page" });
     userEvent.click(nextPageButton);
     expect(pageDisplay.textContent).toBe("Page 2 of 2");
